29351003649d510013c2cb144892a85ddc855384
Alexander Yeagle <AlexanderYeagle@MacBook-Pro.local>
2023-12-24 10:48:51 -0800

    adding account obj and main script wireframe

--- exec_paper_trades.py
+++ exec_paper_trades.py
@@ -16,42 +16,42 @@ print(secret_key)
 trading_client = TradingClient(api_key, secret_key, paper=True)
 
 
-async def main():
+# async def main():
 
 
-    print('client setup')
+print('client setup')
 
-    # preparing market order
-    market_order_data = MarketOrderRequest(
-        symbol="SPY",
-        qty=0.1,
-        side=OrderSide.BUY,
-        time_in_force=TimeInForce.DAY
-    )
-    print('order built')
-    print(market_order_data)
+# preparing market order
+market_order_data = MarketOrderRequest(
+    symbol="SPY",
+    qty=0.1,
+    side=OrderSide.BUY,
+    time_in_force=TimeInForce.DAY
+)
+print('order built')
+print(market_order_data)
 
-    # Market order
-    market_order = trading_client.submit_order(
-        order_data=market_order_data
-    )
-    print(market_order)
-    print('order sent')
+# Market order
+market_order = trading_client.submit_order(
+    order_data=market_order_data
+)
+print(market_order)
+print('order sent')
 
-    # Get the last 100 closed orders
-    get_orders_data = GetOrdersRequest(
-        # status=QueryOrderStatus.CLOSED,
-        limit=100,
-        nested=True  # show nested multi-leg orders
-    )
+# Get the last 100 closed orders
+get_orders_data = GetOrdersRequest(
+    # status=QueryOrderStatus.CLOSED,
+    limit=100,
+    nested=True  # show nested multi-leg orders
+)
 
-    orders = trading_client.get_orders(filter=get_orders_data)
-    # Process orders here
-    print(orders)
+orders = trading_client.get_orders(filter=get_orders_data)
+# Process orders here
+print(orders)
 
 
-if __name__ == "__main__":
-    asyncio.run(main())
+# if __name__ == "__main__":
+#     asyncio.run(main())
 
 # trading_client = TradingClient('api-key', 'secret-key', paper=True)
 

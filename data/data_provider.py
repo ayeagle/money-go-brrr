5a3c53d646737ab64ddaf77ae6a3fcd6b2bbb09a
Alexander Yeagle <AlexanderYeagle@MacBook-Pro.local>
2023-12-26 11:26:19 -0800

    cleaning up some async issues and moving to dataframe standard formatting

--- data/data_provider.py
+++ data/data_provider.py
@@ -9,11 +9,15 @@ from data.class_data_provider_params import DataProviderParams
 from data.class_data_provider_payload import DataProviderPayload
 from consts.data_type_enums import DataSourceFormat
 from alpaca.data.timeframe import TimeFrame
+from alpaca.trading.requests import GetOrdersRequest
+from alpaca.trading.enums import QueryOrderStatus
+from alpaca.trading.client import TradingClient
+import asyncio
 
 
-def stock_price_provider(
+async def gen_stock_prices(
     acc: AlpacaAccount,
-    target_symbol: List[str],
+    target_symbols: List[str],
     period_start: datetime,
     period_end: datetime,
     format: DataSourceFormat = DataSourceFormat.PYDICT
@@ -22,16 +26,16 @@ def stock_price_provider(
     hist_data_client = StockHistoricalDataClient(*acc.get_api_keys())
 
     params_payload = StockQuotesRequest(
-        symbol_or_symbols=target_symbol,
+        symbol_or_symbols=target_symbols,
         start=period_start,
         # end=period_end, TODO something weird going on w/ this param
-        limit=10000,
+        limit=5,
         # timeframe=TimeFrame.Day,
         feed='sip',
         sort='asc',
     )
 
-    hist_data = hist_data_client.get_stock_quotes(params_payload)
+    hist_data = hist_data_client.get_stock_quotes(params_payload).df
 
     formatted_data = None
 
@@ -40,25 +44,68 @@ def stock_price_provider(
     elif (format == DataSourceFormat.JSON):
         formatted_data = json.dumps(hist_data)
     elif (format == DataSourceFormat.DATAFRAME):
-        formatted_data = pd.DataFrame(hist_data[target_symbol])
+        # TODO handle multiple symbols formatting
+        formatted_data = pd.DataFrame(hist_data[target_symbols])
 
     return formatted_data
 
 
-def data_provider(
+async def gen_orders_data(
+    acc: AlpacaAccount,
+    target_symbols: List[str],
+    period_start: datetime,
+    period_end: datetime,
+    paper_trading: bool = True,
+    format: DataSourceFormat = DataSourceFormat.PYDICT
+):
+
+    order_data_client = TradingClient(*acc.get_api_keys(), paper=paper_trading)
+
+    params_payload = GetOrdersRequest(
+        status=QueryOrderStatus.CLOSED,
+        limit=500,  # 500 is max orders returned
+        after=period_start,
+        # until=period_end,  TODO something weird going on w/ this param
+        symbols=target_symbols
+    )
+
+    filled_orders = order_data_client.get_orders(filter=params_payload)
+
+    formatted_data = None
+
+    if (format == DataSourceFormat.PYDICT):
+        formatted_data = filled_orders
+    elif (format == DataSourceFormat.JSON):
+        formatted_data = json.dumps(filled_orders)
+    elif (format == DataSourceFormat.DATAFRAME):
+        # TODO handle multiple symbols formatting
+        formatted_data = pd.DataFrame(filled_orders[target_symbols])
+
+    return formatted_data
+
+
+async def gen_data(
     acc: AlpacaAccount,
     params: DataProviderParams
 ) -> DataProviderPayload:
 
-    stock_data = stock_price_provider(
+    stock_data = await gen_stock_prices(
         acc=acc,
-        target_symbol=params.stock_tickers,
+        target_symbols=params.stock_tickers,
         period_start=params.period_start,
         period_end=params.period_end,
-        format=params.data_format
-    )
+        format=params.data_format)
+    orders_data = await gen_orders_data(
+        acc=acc,
+        target_symbols=params.stock_tickers,
+        period_start=params.period_start,
+        period_end=params.period_end,
+        paper_trading=acc.paper_trading,
+        format=params.data_format)
+
     diff_data_sources = {
-        'stock_data': stock_data
+        'stock_data': stock_data,
+        'orders_data': orders_data
     }
 
     combined_data_sources = diff_data_sources  # TODO actually set this up lol
@@ -68,8 +115,5 @@ def data_provider(
         diff_data_sources=diff_data_sources,
         combined_data_sources=combined_data_sources
     )
-
-    # if(params.get_acc_trade_data):
-    # get data
 
     return final_data_payload
